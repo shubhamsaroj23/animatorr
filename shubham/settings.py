c90b4619782953ef75937ccb718992559a23c375
kothawleprem <kothawleprem@gmail.com>
2020-06-28 20:15:35 +0530

    a

--- shubham/settings.py
+++ shubham/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -125,3 +126,4 @@ STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
+django_heroku.settings(locals())
